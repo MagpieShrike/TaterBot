1cb7e792504cce9749f34103cdddb552feb7a099
Halie Chalkley <magpieshrike@gmail.com>
2021-05-14 13:38:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,30 +11,46 @@ client.once('ready', () => {
 client.on('message', msg => {
     if ( msg.content.toLowerCase().includes( 'raw potato' )) {
         msg.channel.send( 'Get tatered!' );
+        emotion();
+    }
+    if ( msg.content.toLowerCase().includes( 'get tatered' )) {
+        msg.channel.send( 'WOOOO!' );
+        emotion();
     }
     else if ( msg.content.toLowerCase() === 'tater' ) {
         msg.channel.send( 'tot' );
         console.log(msg.author.id)
+        emotion();
     }
     else if ( msg.content.toLowerCase().includes( 'this is stupid' )) {
         msg.channel.send( 'eat a potato about it' );
+        emotion();
     }
     else if ( msg.content.toLowerCase().includes('potato') ) {
         msg.react( '🥔' )
             .then(console.log)
             .catch(console.error);
+        emotion();
     }
 
-    if (msg.author.id === '330691788861997056') {
-        msg.react( '💜' )
-            .then(console.log)
-            .catch(console.error);
+    function emotion() {
+        if (msg.author.id === '330691788861997056') {
+            msg.react( '💜' )
+                .then(console.log)
+                .catch(console.error);
+        }
+        else if ( msg.author.id === '230753391582773248' ) {
+            msg.react( '💛' )
+                .then(console.log)
+                .then(console.error);
+        }
+        else if ( msg.author.id === '237370836527349771' ) {
+            msg.react( '🔥' )
+                .then(console.log)
+                .then(console.error);
+        }
     }
-    else if ( msg.author.id === '' ) {
-        msg.react( '🔥' )
-            .then(console.log)
-            .then(console.error);
-    }
+    
 });
 
 
