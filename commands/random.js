0de87baaded864a37d6d4287fa0ad6fb74c3327c
Halie Chalkley <magpieshrike@gmail.com>
2021-05-14 21:15:19 -0400

    have some patience

---
+++ commands/random.js
@@ -0,0 +1,55 @@
+/*
+const Axios = require('axios');
+const Cheerio = require('cheerio');
+
+const url = "https://www1.flightrising.com/scrying/predict";
+
+function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
+  }
+
+ async function getImage() {
+    const html = await Axios.get(url).then(await sleep(10000));
+    const $ = await Cheerio.load(html.data);
+    let data = [];
+
+    $('.scry-preview').each((i, elem) => {
+        if (i <= 1) {
+            data.push({
+                div: $(elem).find('div').attr('id'),
+                image: $(elem).find('div').find('img').attr('src')
+            })
+        }
+    });
+
+    console.log(data)
+}
+
+getImage();
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    name: 'random',
+    description: 'get a random dragon',
+    execute( msg, args ){
+
+        msg.channel.send( 'COMING SOON' );
+
+    }
+}
--- main.js
+++ main.js
@@ -1,19 +1,51 @@
-import Discord, { Message, MessageAttachment } from 'discord.js';
-import 'dotenv/config';
+
+require('dotenv').config()
+const Discord = require('discord.js');
+const fs = require('fs');
 
 const client = new Discord.Client();
+const prefix = "tot.";
 
+client.commands = new Discord.Collection();
 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 client.once('ready', () => {
     console.log( 'TaterBot is Online' );
 })
 
+// Commands
 client.on('message', msg => {
+    if(!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if( command === 'horoscope' ){
+        client.commands.get('horoscope').execute(msg, args);
+    };
+    /*
+    if( command === 'random' ){
+        client.commands.get('random').execute(msg, args);
+    };
+    */
+});
+
+// Searching
+client.on('message', msg => {
+    // Tater
     if ( msg.content.toLowerCase().includes( 'raw potato' )) {
         msg.channel.send( 'Get tatered!' );
         emotion();
     }
-    if ( msg.content.toLowerCase().includes( 'get tatered' )) {
+    else if ( msg.content.toLowerCase().includes( 'cease' )) {
+        msg.channel.send( 'NEVER!' );
+        emotion();
+    }
+    else if ( msg.content.toLowerCase().includes( 'get tatered' )) {
         msg.channel.send( 'WOOOO!' );
         emotion();
     }
@@ -32,24 +64,43 @@ client.on('message', msg => {
             .catch(console.error);
         emotion();
     }
+    else if ( msg.content.toLowerCase().includes( "still workin' bud?" )) {
+        msg.channel.send( 'yep!' );
+        emotion();
+    }
+    else if ( msg.content === 'AMANDA' ) {
+        msg.channel.send( '💛' );
+        emotion();
+    }
+
+    // Flower
+    if ( msg.content.toLowerCase().includes( 'you need to wait' ) && msg.author.id === '731736201400418314' ) {
+        msg.channel.send( 'Have some patience!' );
+    }
+
+    /*
+    // Randerg
+    if ( msg.content.toLowerCase().includes( 'r' )) {
+        Randerg();
+    }
+    */
+
+
+
+
 
     function emotion() {
         if (msg.author.id === '330691788861997056') {
             msg.react( '💜' )
-                .then(console.log)
-                .catch(console.error);
         }
         else if ( msg.author.id === '230753391582773248' ) {
             msg.react( '💛' )
-                .then(console.log)
-                .then(console.error);
         }
         else if ( msg.author.id === '237370836527349771' ) {
             msg.react( '🔥' )
-                .then(console.log)
-                .then(console.error);
         }
     }
+
     
 });
 
